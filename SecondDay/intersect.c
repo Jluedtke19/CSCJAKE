0362663a9e6b35eee308adb7e72d9b04e669a489
Jluedtke19 <jluedtke19@cornellcollege.edu>
2017-10-03 20:45:11 -0500

    Made a simple intersecet lines

--- SecondDay/intersect.c
+++ SecondDay/intersect.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-#define EPSILON 1E-8
+int main(int argc, char** argv){
+  int n;
+  int points[8];
+  //read in points, two points on first line then next two
+  for(int i =0; i <8; i++){
+    scanf("%d", &n);
+    points[i]=n;
+     }
+  //printf("%d ", points[2]);
+  int a = points[2] - points[0];
+  int b = points[4] - points[6];
+  int c = points[3] - points[1];
+  int d = points[5] - points[7];
+  int e = points[4] - points[0];
+  int f = points[5] - points[1];
+  
+  int s = ((d*e) -(b*f))/((a*d)-(b*c));
+  int t = ((a*f)-(c*e))/((a*d)-(b*c));
+  
+  if( s<=1 & s >= 0  & t<=1 & t >= 0){
+    printf("The lines intersect");
+  }
+  else{
+    printf("The lines don't intersect");
+  }
 
-#define TRUE 1
-#define FALSE 0
-
-struct vector {
-  double x;
-  double y;
 }
---
+++ SecondDay/intersect.c~
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define EPSILON 1E-8
+
+#define TRUE 1
+#define FALSE 0
+
+struct vector {
+  double x;
+  double y;
+}
