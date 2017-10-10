75149b4d9f0fd483e8d70b92a6a3a853ccabcb3f
Jluedtke19 <jluedtke19@cornellcollege.edu>
2017-10-10 08:19:48 -0500

    Chapter5

--- Sceme/points.rkt
+++ Sceme/points.rkt
@@ -2,13 +2,6 @@
 
 ;filter
 
-(define(mreverse list)
-  (define(mreverse2 list list2)
-    (if(null? list)
-       list2
-     (mreverse2 (cdr list) (cons (car list) list2))))
-    ;takes reverse of the rest of the list while putting the head on 2
-   (mreverse2 list '()))
 
 (define(coordinate)
   (/ (truncate(* (random) 100)) 100))
@@ -20,13 +13,14 @@
 (define (xpoint point) (car point))
 (define (ypoint point) (car (cdr point)))
 
-(define (pointlist counter)
-  (define (maker counter result)
+(define (pointmakerlist count)
+  (define( helper list counter)
     (if (equal? counter 0)
-        result
-        (maker ( - counter 1) (cons(point) result))))
-  ;counter is amount of points, put points on result list, go till 0
-  (maker counter '()))
+        list
+        (helper (cons(point) list) (- counter 1))))
+  (helper '() count))
+       
+        
 
 (define (quadrant1 point)
   (and
